862573e99fbf135e877b1b390a4a1562fa4831b9
litongjava <litongjava@qq.com>
2023-10-23 00:36:59 -1000

    修改为使用ViewPage2Service

--- app/src/main/java/com/litongjava/wechat/android/ui/ViewPager2WechatActvity.java
+++ app/src/main/java/com/litongjava/wechat/android/ui/ViewPager2WechatActvity.java
@@ -9,16 +9,15 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.litongjava.android.view.inject.annotation.FindViewById;
 import com.litongjava.android.view.inject.annotation.FindViewByIdLayout;
 import com.litongjava.android.view.inject.annotation.OnClick;
 import com.litongjava.android.view.inject.utils.ViewInjectUtils;
-import com.litongjava.wechat.android.ui.adapter.FragmentViewPager2Adapter;
-import com.litongjava.wechat.android.ui.callback.ViewPager2OnPageChangeCallback;
+import com.litongjava.jfinal.aop.Aop;
+import com.litongjava.jfinal.aop.AopManager;
 import com.litongjava.wechat.android.ui.fragment.EmptyFragment;
+import com.litongjava.wechat.android.ui.services.ViewPage2Service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,15 +32,6 @@ public class ViewPager2WechatActvity extends AppCompatActivity {
   @FindViewById(R.id.viewPager2)
   private ViewPager2 viewPager2;
 
-  @FindViewById(R.id.bottom_weixin_linearLayout)
-  private LinearLayout bottom_weixin_linearLayout;
-  @FindViewById(R.id.bottom_contact_list_linearLayout)
-  private LinearLayout bottom_contact_list_linearLayout;
-  @FindViewById(R.id.bottom_find_linearLayout)
-  private LinearLayout bottom_find_linearLayout;
-  @FindViewById(R.id.bottom_profile_linearLayout)
-  private LinearLayout bottom_profile_linearLayout;
-
   @FindViewById(R.id.bottom_weixin_imageView)
   private ImageView bottom_weixin_imageView;
   @FindViewById(R.id.bottom_contact_list_imageView)
@@ -50,17 +40,6 @@ public class ViewPager2WechatActvity extends AppCompatActivity {
   private ImageView bottom_find_imageView;
   @FindViewById(R.id.bottom_profile_imageView)
   private ImageView bottom_profile_imageView;
-
-  @FindViewById(R.id.bottom_weixin_textView)
-  private TextView bottom_weixin_textView;
-  @FindViewById(R.id.bottom_contact_list_textView)
-  private TextView bottom_contact_list_textView;
-  @FindViewById(R.id.bottom_find_textView)
-  private TextView bottom_find_textView;
-  @FindViewById(R.id.bottom_profile_textView)
-  private TextView bottom_profile_textView;
-
-  List<ImageView> imageViews = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -79,24 +58,21 @@ public class ViewPager2WechatActvity extends AppCompatActivity {
     FragmentManager fragmentManager = getSupportFragmentManager();
     Lifecycle lifecycle = getLifecycle();
     List<Fragment> fragments = getFragments();
-    FragmentViewPager2Adapter adapter = new FragmentViewPager2Adapter(fragmentManager, lifecycle, fragments);
-    viewPager2.setAdapter(adapter);
-
     List<ImageView> imageViews = getImageViews();
-    ViewPager2.OnPageChangeCallback changeCallback = new ViewPager2OnPageChangeCallback(imageViews);
-    viewPager2.registerOnPageChangeCallback(changeCallback);
+
+    ViewPage2Service viewPage2Services = new ViewPage2Service(fragmentManager, lifecycle, viewPager2, fragments, imageViews);
+    AopManager.me().addSingletonObject(viewPage2Services);
 
   }
 
   private List<ImageView> getImageViews() {
-    if (imageViews == null) {
-      imageViews = new ArrayList<>();
-      imageViews.add(bottom_weixin_imageView);
-      imageViews.add(bottom_contact_list_imageView);
-      imageViews.add(bottom_find_imageView);
-      imageViews.add(bottom_profile_imageView);
-    }
+    List<ImageView> imageViews = new ArrayList<>();
+    imageViews.add(bottom_weixin_imageView);
+    imageViews.add(bottom_contact_list_imageView);
+    imageViews.add(bottom_find_imageView);
+    imageViews.add(bottom_profile_imageView);
     return imageViews;
+
   }
 
   private List<Fragment> getFragments() {
@@ -115,38 +91,21 @@ public class ViewPager2WechatActvity extends AppCompatActivity {
 
   @OnClick(R.id.bottom_weixin_imageView)
   public void bottom_weixin_imageView_OnClick(View view) {
-    chageFragment(0);
-
+    Aop.get(ViewPage2Service.class).chageFragment(0);
   }
 
   @OnClick(R.id.bottom_contact_list_imageView)
   public void bottom_contact_list_textView_OnClick(View view) {
-    chageFragment(1);
+    Aop.get(ViewPage2Service.class).chageFragment(1);
   }
 
   @OnClick(R.id.bottom_find_imageView)
   public void bottom_find_textView_OnClick(View view) {
-    chageFragment(2);
+    Aop.get(ViewPage2Service.class).chageFragment(2);
   }
 
   @OnClick(R.id.bottom_profile_imageView)
   public void bottom_profile_textView_OnClick(View view) {
-    chageFragment(3);
-  }
-
-
-  private void chageFragment(int index) {
-    //fragment
-    viewPager2.setCurrentItem(index);
-    //修改按钮状态
-    List<ImageView> imageViews = getImageViews();
-    for (int i = 0; i < imageViews.size(); i++) {
-      ImageView imageView = imageViews.get(i);
-      if (index == i) {
-        imageView.setSelected(true);
-      } else {
-        imageView.setSelected(false);
-      }
-    }
+    Aop.get(ViewPage2Service.class).chageFragment(3);
   }
 }
